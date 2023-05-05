686e6318e4aca0440463bf35aa2bf56538994e6e
mingIxuan <110612843+mingIxuan@users.noreply.github.com>
2023-05-05 08:45:35 -0300

    Add files via upload

---
+++ Assets/Scripts/Controles1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controles1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float speed = 0.1f;
+    float rotationSpeed = -4;
+    public KeyCode teclaAvanzar;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, speed, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -speed, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed, 0, 0);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Instanciator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instanciator : MonoBehaviour
+{
+    public GameObject prefabProyectil;
+    public Transform spawnPointTr;
+    public string buttonString;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown(buttonString))
+        {
+            GameObject clon;
+            clon = Instantiate(prefabProyectil, spawnPointTr.position, Quaternion.identity);
+            Destroy(clon, 1);
+        }
+    }
+}
